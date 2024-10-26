09f5af02307b014dcf136875fe71b37dbbda7f31
ChangeSuger <camille_0315@buaa.edu.cn>
2024-10-26 16:10:31 +0800

    feat: 增加鼠标交互增强示例

--- src/config/sidebar.ts
+++ src/config/sidebar.ts
@@ -2,5 +2,5 @@ import type { SidebarItem } from 'node_modules/@astrojs/starlight/schemas/sideba
 
 export const sidebar: SidebarItem[] = [
   { label: '首页', link: '/' },
-  { label: '第一个示例', link: '/examples/01-first-example/' },
+  { label: '鼠标交互增强', link: '/examples/01-cursor-enhance/' },
 ].map(item => ({...item, translations: {}, attrs: {}}));
